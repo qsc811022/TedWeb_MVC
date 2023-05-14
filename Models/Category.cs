a3cc9cd8048fb9952c1a7468d990aa6429febc6d
劉敦桐 <zse811022@hotmail.com>
2023-05-14 17:09:02 +0800

    Setting project

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TedWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string DisplayOrder{ get; set; }
+
+    }
+}
