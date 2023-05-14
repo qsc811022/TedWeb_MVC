9bda69f5af99e396dc15a3cc761159c32c057312
劉敦桐 <zse811022@hotmail.com>
2023-05-14 21:41:12 +0800

    add-migration AddCategoryTableToDb

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TedWeb.Models;
+
+namespace TedWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
