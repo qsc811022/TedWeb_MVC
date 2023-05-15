6209ac186694ae15b03f61ee3c8ac9e8384d1c7d
劉敦桐 <zse811022@hotmail.com>
2023-05-15 21:53:15 +0800

    add View /Category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TedWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
