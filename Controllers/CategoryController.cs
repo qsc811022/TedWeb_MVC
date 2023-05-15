b439d2b60b264f440bba98ad816d107497813d39
劉敦桐 <zse811022@hotmail.com>
2023-05-16 07:18:51 +0800

    show all data in index.cshtml

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using TedWeb.Data;
+using TedWeb.Models;
+
 namespace TedWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category>objCategoryList=_db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
