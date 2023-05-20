26c5ce6156c85174a022268d4caa580fcb23bd04
劉敦桐 <zse811022@hotmail.com>
2023-05-20 08:29:14 +0800

    Custom Validations

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -25,6 +25,10 @@ namespace TedWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","Thse DisaplyOrder cannot exactly match the Name");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
