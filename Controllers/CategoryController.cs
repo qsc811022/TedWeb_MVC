2fe5dfbafb6ce2c954aaaeea40ea6916b9a8deac
劉敦桐 <zse811022@hotmail.com>
2023-05-21 12:50:45 +0800

    add Edit Function

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -63,17 +63,9 @@ namespace TedWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "Thse DisaplyOrder cannot exactly match the Name");
-            }
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "Thse DisaplyOrder cannot exactly match the Name");
-            //}
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
