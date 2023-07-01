77cc8ead199c978d801f45d6256125b75448abf9
劉敦桐 <zse811022@hotmail.com>
2023-07-01 11:31:12 +0800

    add   dataSrc: 'objProductList'  bug

---
+++ wwwroot/js/product.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        ajax: {
+            url: '/admin/Product/getall',
+            dataSrc: 'objProductList'
+        },
+        "columns": [
+            { data: 'title', "width": "20%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "15%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
+
+
