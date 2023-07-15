f1ae3dd4850e806095668262103bc2f4f842156c
劉敦桐 <zse811022@hotmail.com>
2023-07-15 22:14:41 +0800

    ADD identity

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 using TedWeb.DataAccess.Repository;
 using TedWeb.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 //builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
