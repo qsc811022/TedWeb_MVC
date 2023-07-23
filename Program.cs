c06957ce874fa4fead45ac62b9bfe5a23dddbb94
劉敦桐 <zse811022@hotmail.com>
2023-07-23 15:50:24 +0800

    fix bug

--- Program.cs
+++ Program.cs
@@ -19,6 +19,14 @@ builder.Services.AddRazorPages();
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
+builder.Services.ConfigureApplicationCookie(optione =>
+{
+    optione.LoginPath=$"/Identity/Account/Login";
+    optione.LogoutPath = $"/Identity/Account/Logout";
+    optione.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender,EmailSender>();
 
