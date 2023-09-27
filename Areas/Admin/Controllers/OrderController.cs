9b3ff404311fe7e248eccbaea6fc63d326a90417
劉敦桐 <zse811022@hotmail.com>
2023-09-27 22:10:13 +0800

    1123

--- Areas/Admin/Controllers/OrderController.cs
+++ Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System.Security.Claims;
 using TedWeb.DataAccess.Repository.IRepository;
 using TedWeb.Model;
+using TedWeb.Model.ViewModels;
+using TedWeb.Utility;
 
 namespace TedWeb.Areas.Admin.Controllers
 {
@@ -20,16 +22,66 @@ namespace TedWeb.Areas.Admin.Controllers
 			return View();
 		}
 
-		#region API Calls
-		[HttpGet]
-		public IActionResult GetAll() {
-			List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser").ToList();
-			return Json(new {data=objOrderHeader});
-		}
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+            };
 
-		#endregion
+            return View(orderVM);
+        }
 
 
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> objOrderHeaders;
 
-	}
+
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeaders = _unitOfWork.OrderHeader
+                    .GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+
+            }
+
+
+            return Json(new { data = objOrderHeaders });
+        }
+
+
+        #endregion
+    }
 }
+
+	
